6852a1ac821caf36701cf4d02efb381e11d76171
Guilherme Freire <gfreire1992@gmail.com>
2016-10-09 19:13:13 -0300

    Added accelerometer plugin and events

--- MyTest/www/js/index.js
+++ MyTest/www/js/index.js
@@ -50,12 +50,59 @@ var app = {
 
 app.initialize();
 
-  document.addEventListener('deviceready', function() {
-      var socket = io('http://192.168.0.10:8000');
+// My vars
+
+var socket
+
+var spin  = {'client_type':'device','x':0.01,'y':0,'z':0}
+var spin2 = {'client_type':'device','x':0,'y':0,'z':0.05}
+var spin3 = {'client_type':'device','x':-0.01,'y':0,'z':-0.05}
+
+
+
+document.addEventListener('deviceready', function() {
+
+    socket = io('http://192.168.0.10:8000');
     socket.on('connect', function() {
-      socket.emit('text_move', {'client_type':'device','x':0.1,'y':0,'z':0}); 
-      socket.on('text', function(text) {
-        // alert(text);
-       });
-     });
-  });
+        // socket.emit('order_move', spin); 
+        socket.on('doge', function(text) {
+            // alert(text);
+        });
+    });
+});
+
+
+function onSuccess(acceleration) {
+    socket.emit('order_print', '~')
+    socket.emit('order_print', 'Acceleration X: ' + acceleration.x)
+    socket.emit('order_print', 'Acceleration Y: ' + acceleration.y)
+    socket.emit('order_print', 'Acceleration Z: ' + acceleration.z)
+    // socket.emit('order_print', 'Timestamp: '      + acceleration.timestamp)
+}
+
+function onError() {
+    socket.emit('order_print', 'onError!');
+}
+var options = { frequency: 1000 };
+
+
+var watchID = null;
+
+function doSpin(move) {
+    if (watchID == null) {
+        watchID = navigator.accelerometer.watchAcceleration(
+            onSuccess, 
+            onError, 
+            options
+        );
+    };
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    socket.emit('order_move', move); 
+    // window.open("http://www.w3schools.com", "_self");
+}
+
+function doReset() {
+    navigator.accelerometer.clearWatch(watchID);
+    watchID = null;
+    socket.emit('order_reset'); 
+}
--- client_server/index.js
+++ client_server/index.js
@@ -19,19 +19,43 @@ io.on('connection', function (socket) {
   console.log('socket connected >>>', socket.id);
   // console.log(socket);
 
-  socket.emit('text', 'wow. such event. very real time.');
-  socket.on('text_move', function (data) {
-    console.log('text_move', data);
+  socket.emit('doge', 'wow. such event. very real time.');
+
+
+
+  // Cordova
+  socket.on('order_move', function (data) {
+    console.log('order_move', data);
 
     for (var key in socket_dict) {
       if (socket_dict[key].connected) {
-        console.log(key);
+        // console.log(key);
         socket_dict[key].emit('move', data);
-      };
+      }
     }
   });
 
+  socket.on('order_reset', function () {
+    console.log('order_reset');
+
+    for (var key in socket_dict) {
+      if (socket_dict[key].connected) {
+        socket_dict[key].emit('reset');
+      }
+    }
+  });
+
+  socket.on('order_print', function (data) {
+    console.log(data);
+  })
+
+
+
+
   socket.on('move', function (data) {
     socket.emit('move', data);
+  });
+  socket.on('reset', function () {
+    socket.emit('reset');
   });
 });
