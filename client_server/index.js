1d1688aed1e2691cd5688332ece165be72bb1dc1
Guilherme Freire <gfreire1992@gmail.com>
2016-10-08 23:02:20 -0300

    Added minimal cordova project

--- client_server/index.js
+++ client_server/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -12,28 +12,26 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
+var socket_dict = {};
+
 io.on('connection', function (socket) {
-  console.log('socket connected');
+  socket_dict[socket.id] = socket;
+  console.log('socket connected >>>', socket.id);
+  // console.log(socket);
+
   socket.emit('text', 'wow. such event. very real time.');
+  socket.on('text_move', function (data) {
+    console.log('text_move', data);
+
+    for (var key in socket_dict) {
+      if (socket_dict[key].connected) {
+        console.log(key);
+        socket_dict[key].emit('move', data);
+      };
+    }
+  });
+
   socket.on('move', function (data) {
     socket.emit('move', data);
   });
 });
-
-
-
-
-// var server = require('http').createServer();
-// var io = require('socket.io')(server);
-
-// io.sockets.on('connection', function (socket) {
-//     console.log('socket connected');
-
-//     socket.on('disconnect', function () {
-//         console.log('socket disconnected');
-//     });
-
-//     socket.emit('text', 'wow. such event. very real time.');
-// });
-
-// server.listen(3000);
